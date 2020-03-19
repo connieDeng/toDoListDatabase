6d84f675be3dd320fc1afd870e21c60c955d594d
connieDeng <dengconnie0723@gmail.com>
2020-03-19 11:59:16 -0400

    debugging

--- connection.js
+++ connection.js
@@ -2,23 +2,13 @@ const mysql = require('mysql');
 const dbConfig = require("./db.config.js");
 
 // database configuration ===============================================================
-var db = mysql.createPool({
+var connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
 });
 
+connection.end();
 
-db.connect((err) => {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    
-    console.error('you have connected to the database successfully!');
-});
-
-db.end();
-
-module.exports = db;
+module.exports = connection;
